00ab0b3aa4e177b53d8d5a0c218059bd5759896d
Phan Ngoc Tuan <ngoctuan@MacBook-Pro-cua-Phan.local>
2019-06-21 22:22:09 +0700

    [Section 1] CLI Console Command

--- app/code/SimplifiedMagento/FirstModule/Model/HeavyService.php
+++ app/code/SimplifiedMagento/FirstModule/Model/HeavyService.php
@@ -4,11 +4,21 @@ namespace SimplifiedMagento\FirstModule\Model;
 
 class HeavyService
 {
+    /**
+     * HeavyService construct
+     * 
+     * @return void
+     */
     public function __construct()
     {
         echo "HeavyService has been instantiated" . "</br>";
     }
 
+    /**
+     * Print heavy service message
+     * 
+     * @return void
+     */
     public function printHeavyServiceMessage()
     {
         echo "Message from HeavyService class";
