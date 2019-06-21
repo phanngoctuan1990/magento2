c9c49093b36b664fd96307a372edceac84006efd
Phan Ngoc Tuan <ngoctuan@MacBook-Pro-cua-Phan.local>
2019-06-21 21:00:40 +0700

    [Section 1] Proxies

--- app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
+++ app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -2,16 +2,20 @@
 
 namespace SimplifiedMagento\FirstModule\Controller\Page;
 
+use Magento\Framework\App\Request\Http;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Catalog\Model\ProductFactory;
 use Magento\Framework\Event\ManagerInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use SimplifiedMagento\FirstModule\Model\HeavyService;
 use SimplifiedMagento\FirstModule\Api\PencilInterface;
 use SimplifiedMagento\FirstModule\Model\PencilFactory;
 
 class HelloWorld extends Action
 {
+    protected $http;
+    protected $heavyService;
     protected $pencilFactory;
     protected $_eventManager;
     protected $productFactory;
@@ -21,7 +25,9 @@ class HelloWorld extends Action
     /**
      * HelloWorld construct
      * 
+     * @param Http                       $http              http
      * @param Context                    $context           context
+     * @param HeavyService               $heavyService      heavyService
      * @param PencilFactory              $pencilFactory     pencilFactory
      * @param ProductFactory             $productFactory    productFactory
      * @param ManagerInterface           $_eventManager     _eventManager
@@ -31,7 +37,9 @@ class HelloWorld extends Action
      * @return void
      */
     public function __construct(
+        Http $http,
         Context $context,
+        HeavyService $heavyService,
         PencilFactory $pencilFactory,
         ProductFactory $productFactory,
         ManagerInterface $_eventManager,
@@ -39,6 +47,8 @@ class HelloWorld extends Action
         ProductRepositoryInterface $productRepository
     ) {
         parent::__construct($context);
+        $this->http = $http;
+        $this->heavyService = $heavyService;
         $this->pencilFactory = $pencilFactory;
         $this->_eventManager = $_eventManager;
         $this->productFactory = $productFactory;
@@ -57,26 +67,33 @@ class HelloWorld extends Action
     public function execute()
     {
         // echo get_class($this->productRepository);
-        echo $this->pencilInterface->getPencilType() . "</br>";
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $pencil = $objectManager->create('SimplifiedMagento\FirstModule\Model\Pencil');
-        var_dump($pencil);
-        $book = $objectManager->create('SimplifiedMagento\FirstModule\Model\Book');
-        echo $book->getBookType() . "</br>";
-        var_dump($book);
-        $student = $objectManager->create('SimplifiedMagento\FirstModule\Model\Student');
-        var_dump($student);
-        $pencil = $this->pencilFactory->create(['name' => 'Bob', 'school' => 'Bach khoa Da Nang']);
-        var_dump($pencil);
-        $product = $this->productFactory->create()->load(1);
-        $product->setName("Iphone XR");
-        $productName = $product->getName();
-        echo $productName . "</br>";
-        $id = $product->getIdBySku('MH02-XL-Red');
-        echo $id . "</br>";
-        echo "Main function" . "</br>";
-        $message = new \Magento\Framework\DataObject(['greeting' => 'Good morning']);
-        $this->_eventManager->dispatch('custom_event', ['greeting' => $message]);
-        echo $message->getGreeting() . "</br>";
+        // echo $this->pencilInterface->getPencilType() . "</br>";
+        // $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        // $pencil = $objectManager->create('SimplifiedMagento\FirstModule\Model\Pencil');
+        // var_dump($pencil);
+        // $book = $objectManager->create('SimplifiedMagento\FirstModule\Model\Book');
+        // echo $book->getBookType() . "</br>";
+        // var_dump($book);
+        // $student = $objectManager->create('SimplifiedMagento\FirstModule\Model\Student');
+        // var_dump($student);
+        // $pencil = $this->pencilFactory->create(['name' => 'Bob', 'school' => 'Bach khoa Da Nang']);
+        // var_dump($pencil);
+        // $product = $this->productFactory->create()->load(1);
+        // $product->setName("Iphone XR");
+        // $productName = $product->getName();
+        // echo $productName . "</br>";
+        // $id = $product->getIdBySku('MH02-XL-Red');
+        // echo $id . "</br>";
+        // echo "Main function" . "</br>";
+        // $message = new \Magento\Framework\DataObject(['greeting' => 'Good morning']);
+        // $this->_eventManager->dispatch('custom_event', ['greeting' => $message]);
+        // echo $message->getGreeting() . "</br>";
+        $id = $this->http->getParam('id', 0);
+
+        if ($id == 1) {
+            $this->heavyService->printHeavyServiceMessage();
+        } else {
+            echo "Heavy service not used";
+        }
     }
 }
---
+++ app/code/SimplifiedMagento/FirstModule/Model/HeavyService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SimplifiedMagento\FirstModule\Model;
+
+class HeavyService
+{
+    public function __construct()
+    {
+        echo "HeavyService has been instantiated" . "</br>";
+    }
+
+    public function printHeavyServiceMessage()
+    {
+        echo "Message from HeavyService class";
+    }
+}
